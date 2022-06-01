a93f0561e903c28e2e1b1f7a4df3c502001aabb6
leonardo.salvador <leonardo.pokrewecky@gmail.com>
2022-06-01 20:55:49 -0300

    Criado a Propriedade e os Métodos ObterUidadeDeMilharPorExtenso implementado parcial.

--- TrabalhoOrientacaoObjetos01/Questao01/Numero.cs
+++ TrabalhoOrientacaoObjetos01/Questao01/Numero.cs
@@ -8,6 +8,83 @@ namespace TrabalhoOrientacaoObjetos01.Questao01
 {
     public class Numero
     {
+        //Propriedades
+        public double Valor;
+        public double ValorAuxiliar;
 
+        public string ValorPorExtenso;
+
+        //Métodos
+        public void ObterDecimalPorExtenso()
+        {
+
+        }
+        public void ObterUnidadePorExtenso()
+        {
+
+        }
+        public void ObterDezenaPorExtenso()
+        {
+
+        }
+        public void ObterCentenaPorExtenso()
+        {
+            
+            ObterDezenaPorExtenso();
+        }
+        public void ObterUidadeDeMilharPorExtenso()
+        {
+            if (ValorAuxiliar >= 1000.00 && ValorAuxiliar <= 1999.99)
+            {
+                ValorPorExtenso = "Mil ";
+                ValorAuxiliar = -1000.00;
+            }
+            else if (ValorAuxiliar <= 2999.99)
+            {
+                ValorPorExtenso = "Dois Mil ";
+                ValorAuxiliar = -2000.00;
+            }
+            else if (ValorAuxiliar <= 3999.99)
+            {
+                ValorPorExtenso = "Três Mil ";
+                ValorAuxiliar = -3000.00;
+            }
+            else if (ValorAuxiliar <= 4999.99)
+            {
+                ValorPorExtenso = "Quatro Mil ";
+                ValorAuxiliar = -4000.00;
+            }
+            else if (ValorAuxiliar <= 5999.99)
+            {
+                ValorPorExtenso = "Cinco Mil ";
+                ValorAuxiliar = -5000.00;
+            }
+            else if (ValorAuxiliar <= 6999.99)
+            {
+                ValorPorExtenso = "Seis Mil ";
+                ValorAuxiliar = -6000.00;
+            }
+            else if (ValorAuxiliar <= 7999.99)
+            {
+                ValorPorExtenso = "Sete Mil ";
+                ValorAuxiliar = -7000.00;
+            }
+            else if (ValorAuxiliar <= 8999.99)
+            {
+                ValorPorExtenso = "Outro Mil ";
+                ValorAuxiliar = -8000.00;
+            }
+            else if (ValorAuxiliar <= 9999.99)
+            {
+                ValorPorExtenso = "Nove Mil ";
+                ValorAuxiliar = -9000.00;
+            }
+            ObterCentenaPorExtenso();
+        }
+        public void ObterNumeroCompletoPorExtenso()
+        {
+            ValorAuxiliar = Valor;
+            ObterUidadeDeMilharPorExtenso();
+        }
     }
 }
