01126ce6c1805b91580765df3d550a3dc7cb60bf
Efraim <mertensefraim@gmail.com>
2022-06-02 18:39:30 -0300

    Criado teste da classe Calendario

---
+++ TrabalhoOrientacaoObjetos01.Tests/Questao02/CalendarioTeste.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrabalhoOrientacaoObjetos01.Questao02;
+using Xunit;
+
+namespace TrabalhoOrientacaoObjetos01.Tests.Questao02
+{
+    public class CalendarioTeste
+    {
+        [Theory]
+        [InlineData("2005/01/24", "janeiro")]
+        public void Validar_ObterMesPorExtenso(string mes, string resultado)
+        {
+            // Arrange
+            var calendario = new Calendario();
+            calendario.Data = Convert.ToDateTime(mes);
+
+            // Act
+            var obterMes = calendario.ObterMesPorExtenso();
+        }
+    }
+}
