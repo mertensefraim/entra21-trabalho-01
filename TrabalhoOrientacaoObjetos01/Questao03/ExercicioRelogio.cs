18ca7c7ff70c39239f688eca280d768b71902bfa
Wellington Scaburri <wscaburri@gmail.com>
2022-06-02 02:11:20 -0300

    Implementação dos métodos da Questão 03

--- TrabalhoOrientacaoObjetos01/Questao03/ExercicioRelogio.cs
+++ TrabalhoOrientacaoObjetos01/Questao03/ExercicioRelogio.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TrabalhoOrientacaoObjetos01.Questao03
+﻿namespace TrabalhoOrientacaoObjetos01.Questao03
 {
     public class ExercicioRelogio
     {
         public void Executar()
         {
+            var relogio = new Relogio();
 
+            Console.Write("Informe a hora: ");
+            relogio.Hora = Convert.ToDateTime(Console.ReadLine());
+
+            var horaCompletaExtenso = relogio.ObterHoraCompletaPorExtenso();
+
+            Console.WriteLine(horaCompletaExtenso);
         }
     }
 }
