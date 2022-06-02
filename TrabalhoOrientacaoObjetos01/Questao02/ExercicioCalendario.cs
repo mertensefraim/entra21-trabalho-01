c4d2139aee374695f49255429148bf8de9367250
Efraim Mertens <mertensefraim@gmail.com>
2022-06-01 22:03:52 -0300

    Modificações feitas na função calendario e pedido de informações básicos para o usuário

--- TrabalhoOrientacaoObjetos01/Questao02/ExercicioCalendario.cs
+++ TrabalhoOrientacaoObjetos01/Questao02/ExercicioCalendario.cs
@@ -10,8 +10,12 @@ namespace TrabalhoOrientacaoObjetos01.Questao02
     {
         public void Executar()
         {
+            var calendario = new Calendario();
+            Console.Write("Digite uma data. Obs.: deve estar no formato ano/mes/dia: ");
+            calendario.Data = Convert.ToDateTime(Console.ReadLine());
 
-
+            Console.WriteLine($"A data digitada foi {calendario.Data.ToString("dd/MM/yyyy")}" +
+                $"\nEla por extenso fica: {calendario.ObterDataCompletaPorExtenso()}");
         }
     }
 }
