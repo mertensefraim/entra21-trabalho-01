cf3f1bbee08f8c4668a3dc0e6fb9909bd4f7d72b
Efraim <mertensefraim@gmail.com>
2022-06-02 21:57:31 -0300

    Program com a verificação

--- TrabalhoOrientacaoObjetos01/Program.cs
+++ TrabalhoOrientacaoObjetos01/Program.cs
@@ -6,29 +6,53 @@ var escolhaUsuario = 0;
 
 while (escolhaUsuario != 99)
 {
+    Console.Clear();
     Console.Write(@"------Questões----------
-Questão 01 - 01
-Questão 02 - 02
-Questão 03 - 03
-Sair - 99
+01 - Questão 01
+02 - Questão 02
+03 - Questão 03
+99 - Sair
 Escolha a questão desejada: ");
-    escolhaUsuario = Convert.ToInt32(Console.ReadLine());
 
-    if (escolhaUsuario == 1)
-    {
-        var questao = new ExercicioNumero();
-        questao.Executar();
-    }
-    else if (escolhaUsuario == 2)
-    {
-        var questao = new ExercicioCalendario();
-        questao.Executar();
-    }
-    else if (escolhaUsuario == 3)
-    {
-        var questao = new ExercicioRelogio();
-        questao.Executar();
-    }
+    var validar = false;
 
-    
+    while (validar == false)
+    {
+        try
+        {
+            escolhaUsuario = Convert.ToInt32(Console.ReadLine());
+
+            if (escolhaUsuario == 1)
+            {
+                var questao = new ExercicioNumero();
+                questao.Executar();
+            }
+            else if (escolhaUsuario == 2)
+            {
+                var questao = new ExercicioCalendario();
+                questao.Executar();
+            }
+            else if (escolhaUsuario == 3)
+            {
+                var questao = new ExercicioRelogio();
+                questao.Executar();
+            }
+
+            else if (escolhaUsuario == 99)
+                validar = true;
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("O valor digitado não é uma opção válida!");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("O valor deve ser um inteiro");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
 }
